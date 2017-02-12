d8442cfafa645b9a42a78e26ee920d32d5cf323b
rugead <rudolf.vogel@icando.de>
2017-02-12 22:22:14 +0100

    change some stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var babelify = require('babelify');
 var exorcist = require('exorcist');
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
+var ghPages = require('gulp-gh-pages');
 
 function bundle (bundler) {
   return bundler
@@ -74,6 +75,11 @@ gulp.task('watch', function () {
       port: 3010
   });
 
+});
+
+gulp.task('gh', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('default', ['css', 'pug', 'watch']);
