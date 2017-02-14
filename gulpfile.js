b5aa183ec8c6ef96b216e3ed7e43031e090a07c3
rugead <rudolf.vogel@icando.de>
2017-02-14 12:08:21 +0100

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -9,77 +9,83 @@ var exorcist = require('exorcist');
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var ghPages = require('gulp-gh-pages');
+const autoprefixer = require('gulp-autoprefixer');
 
-function bundle (bundler) {
-  return bundler
-    // .transform(babelify, {presets: ["es2015"]})
-    .bundle()
-    .on('error', function (e){
-      gulputil.log(e.message);
+function bundle(bundler) {
+    return bundler
+      // .transform(babelify, {presets: ["es2015"]})
+      .bundle()
+      .on('error', function (e) {
+        gulputil.log(e.message);
     })
-    .pipe(exorcist('./build/js/dist/app.js.map'))
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./build/js/dist'))
-    .pipe(browserSync.stream());
+      .pipe(exorcist('./build/js/dist/app.js.map'))
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./build/js/dist'))
+      .pipe(browserSync.stream());
 }
 
 const srcPaths = {
-  pug: './app/*.pug',
-  css: './app/css/*.css'
+    pug: './app/*.pug',
+    css: './app/css/*.css'
 };
 
 const buildPaths = {
-  build: './build',
-  pug: './build/'
+    build: './build',
+    pug: './build/'
 };
 
-gulp.task('css', function() {
-  return gulp.src(srcPaths.css)
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('build/css'));
+gulp.task('css', function () {
+    return gulp.src(srcPaths.css)
+      .pipe(cleanCSS({ compatibility: 'ie8' }))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+       }))
+      .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('pug', () => {
-  gulp.src(srcPaths.pug)
-    .pipe(pug())
-    .pipe(gulp.dest(buildPaths.pug));
+    gulp.src(srcPaths.pug)
+      .pipe(pug())
+      .pipe(gulp.dest(buildPaths.pug));
 });
 
 gulp.task('js', function () {
-  return bundle(browserify('./app/js/app.js'));
+    return bundle(browserify('./app/js/app.js'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(srcPaths.pug, ['pug']);
-  gulp.watch(srcPaths.css, ['css']);
+    gulp.watch(srcPaths.pug, ['pug']);
+    gulp.watch(srcPaths.css, ['css']);
 
-  watchify.args.debug = true;
-  var watcher = watchify(browserify('./app/js/app.js', watchify.args).transform(babelify, {presets: ["es2015"]}));
+    watchify.args.debug = true;
+    var watcher = watchify(browserify('./app/js/app.js', watchify.args).transform(babelify, { presets: ['es2015'] }));
 
-  bundle(watcher);
-
-  watcher.on('update', function() {
     bundle(watcher);
-  });
 
-  watcher.on('log', gulputil.log);
+    watcher.on('update', function () {
+        bundle(watcher);
+    });
 
-  var files = [
-        buildPaths.build
-  ];
+    watcher.on('log', gulputil.log);
 
-  browserSync.init(files, {
-      browser: "chrome",
-      server: "./build",
-      logFileChange: false,
-      port: 3010
-  });
+    var files = [
+          buildPaths.build
+    ];
+
+    browserSync.init(files, {
+        browser: 'chrome',
+        server: './build',
+        logFileChange: false,
+        port: 3010
+    });
 
 });
 
-gulp.task('gh', function() {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
+gulp.task('gh', function () {
+    return gulp.src('./build/**/*')
+      .pipe(ghPages());
 });
 
+gulp.task('deploy', ['pug', 'js', 'css', 'gh']);
 gulp.task('default', ['css', 'pug', 'watch']);
